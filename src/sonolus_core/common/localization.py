c51d1e7166d50d3c34536e0275f4b2f7f2e86b63
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-01-14 05:00:53 +0900

    🔨 Add localize function

---
+++ src/sonolus_core/common/localization.py
@@ -0,0 +1,12 @@
+def localize(texts: dict, locale: str, fallback_locale: str) -> str:
+    """
+    Localize a text using the given locale.
+    If the locale is not found, fallback to the given locale.
+    If the fallback locale is not found, return the first element of text.
+    """
+    if locale in texts:
+        return texts[locale]
+    elif fallback_locale in texts:
+        return texts[fallback_locale]
+    else:
+        return texts[texts.keys()[0]]
