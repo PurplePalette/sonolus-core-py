43e1bb3cccd78aa052fbc0af7ae4b880b878e1d1
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-01-16 20:20:35 +0900

    ✅ Add localization test

---
+++ tests/unit/test_localization.py
@@ -0,0 +1,40 @@
+from src.sonolus_core.database import (
+    LocalizationText,
+    localize
+)
+
+
+class TestLocalization():
+    def test_localize_with_specify_lang(self) -> None:
+        texts: LocalizationText = {
+            "en": "chino",
+            "ja": "チノ"
+        }
+        localized_text = localize(
+            texts,
+            "en",
+            "ja"
+        )
+        assert localized_text == "chino"
+
+    def test_localize_with_undefined_locale(self) -> None:
+        texts: LocalizationText = {
+            "ja": "チノ"
+        }
+        localized_text = localize(
+            texts,
+            "en",
+            "ja"
+        )
+        assert localized_text == "チノ"
+
+    def test_localize_with_undefined_fallback(self) -> None:
+        texts: LocalizationText = {
+            "ja": "チノ"
+        }
+        localized_text = localize(
+            texts,
+            "en",
+            "en"
+        )
+        assert localized_text == "チノ"
